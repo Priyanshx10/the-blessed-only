acceaabf4cdc37ac0a25ad040a8b69c84eddf810
Priyansh <Priyanshyadav1012@gmail.com>
2024-03-28 04:47:41 +0530

    Complete

--- components/component/Footer.tsx
+++ components/component/Footer.tsx
@@ -1,5 +1,10 @@
 import Link from "next/link"
 import Image from "next/image"
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaSlack } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 export function Footer() {
   return (
@@ -18,11 +23,11 @@ export function Footer() {
               Making the world a better place through constructing elegant hierarchies.
             </p>
             <div className="flex space-x-4">
-              <FacebookIcon className="h-6 w-6 text-gray-400" />
-              <InstagramIcon className="h-6 w-6 text-gray-400" />
-              <SlackIcon className="h-6 w-6 text-gray-400" />
-              <TwitterIcon className="h-6 w-6 text-gray-400" />
-              <YoutubeIcon className="h-6 w-6 text-gray-400" />
+              <FaFacebook  className="h-6 w-6 text-gray-400" />
+              <FaInstagram className="h-6 w-6 text-gray-400" />
+              <FaSlack className="h-6 w-6 text-gray-400" />
+              <FaXTwitter className="h-6 w-6 text-gray-400" />
+              <FaYoutube className="h-6 w-6 text-gray-400" />
             </div>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:grid-cols-4">
@@ -95,134 +100,4 @@ export function Footer() {
     </footer>
   )
 }
-
-
-function FlagIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z" />
-      <line x1="4" x2="4" y1="22" y2="15" />
-    </svg>
-  )
-}
-
-
-function FacebookIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
-    </svg>
-  )
-}
-
-
-function InstagramIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
-      <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
-      <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
-    </svg>
-  )
-}
-
-
-function SlackIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <rect width="3" height="8" x="13" y="2" rx="1.5" />
-      <path d="M19 8.5V10h1.5A1.5 1.5 0 1 0 19 8.5" />
-      <rect width="3" height="8" x="8" y="14" rx="1.5" />
-      <path d="M5 15.5V14H3.5A1.5 1.5 0 1 0 5 15.5" />
-      <rect width="8" height="3" x="14" y="13" rx="1.5" />
-      <path d="M15.5 19H14v1.5a1.5 1.5 0 1 0 1.5-1.5" />
-      <rect width="8" height="3" x="2" y="8" rx="1.5" />
-      <path d="M8.5 5H10V3.5A1.5 1.5 0 1 0 8.5 5" />
-    </svg>
-  )
-}
-
-
-function TwitterIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
-    </svg>
-  )
-}
-
-
-function YoutubeIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17" />
-      <path d="m10 15 5-3-5-3z" />
-    </svg>
-  )
-}
+export default Footer;
