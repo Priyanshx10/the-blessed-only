613d6018464c09794c832f61c0b69b2840648f09
Priyansh <Priyanshyadav1012@gmail.com>
2024-03-28 01:14:39 +0530

    Header Done.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import BlessedHeader from "./BlessedHeader";
+import Component from "./Component";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       
       <body className={inter.className}>
-      <BlessedHeader />
+      <Component />
         {children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      Hello World
+    <div className="w-full"> 
+      <Image
+        src="/merch.png"
+        alt="Main"
+        width={2500}
+        height={1000}
+        className="rounded shadow-md "
+      />
     </div>
   );
 }
