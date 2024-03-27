2b272df884ff3ea2e334dbc84c432356728a5023
Priyansh <Priyanshyadav1012@gmail.com>
2024-03-28 01:03:28 +0530

    Header Done.

---
+++ app/BlessedHeader.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Component from './Component'
+
+
+const BlessedHeader = () => {
+  return (
+    <div className='w-full'>
+      <Component />
+    </div>
+  )
+}
+
+export default BlessedHeader
